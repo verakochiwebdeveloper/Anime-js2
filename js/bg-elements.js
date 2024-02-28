81327caee51ff3fc559d7dd02c1eb04f4fb28f2a
SIARHEI TSYKHUN~ <frontend4order@gmail.com>
2024-02-28 15:40:53 +0300

    fix

--- js/bg-elements.js
+++ js/bg-elements.js
@@ -1,11 +1,13 @@
 const elements = document.querySelectorAll('.set-bg');
 
 function forAnimeElements () {
-    for(let i = 0; i < elements.length; i++) {
-        const srcAnimeElements = elements[i].dataset.setbg;
+    
 
-        elements[i].style.backgroundImage =`url(${srcAnimeElements})`
-    }
+    elements.forEach((elem) => {
+
+        elem.style.backgroundImage =`url(${elem.dataset.setbg})`
+    })
+    
 }
 
 forAnimeElements()
--- js/main-data.js
+++ js/main-data.js
@@ -1,12 +1,37 @@
 const mainData = () => {
-    fetch('https://animebase-fce2a-default-rtdb.europe-west1.firebasedatabase.app/anime.json')
+  const renderTopAnime = (array) => {
+    const wrapper = document.querySelector(".filter__gallery");
+
+    wrapper.innerHTML = "";
+
+    array.forEach((item) => {
+      wrapper.insertAdjacentHTML(
+        "afterbegin",
+        `
+            <div class="product__sidebar__view__item set-bg mix day years"
+                data-setbg="${item.image}">
+                <div class="ep">${item.rating} / 10</div>
+                <div class="view"><i class="fa fa-eye"></i> ${item.views}</div>
+                <h5><a href="/anime-details.html">${item.title}</a></h5>
+            </div>
+        `
+      );
+    });
+    wrapper.querySelectorAll('.set-bg').forEach((elem) => {
+
+        elem.style.backgroundImage =`url(${elem.dataset.setbg})`
+    })
+  };
+
+  fetch(
+    "https://animebase-fce2a-default-rtdb.europe-west1.firebasedatabase.app/anime.json"
+  )
     .then((response) => {
-        return response.json()
+      return response.json();
     })
-    .then((data) =>{
-        console.log(data.anime)
-    })
+    .then((data) => {
+      renderTopAnime(data.sort((a, b) => b.views - a.views).slice(0, 5));
+    });
+};
 
-}
-
-mainData()
+mainData();
